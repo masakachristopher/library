c48407a90c4e8c42992f428eb3bf701fa5f17a72
masakachristopher <50275918+masakachristopher@users.noreply.github.com>
2019-06-05 11:31:34 +0300

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,3 +1,4 @@
+
 <h2>Quick Contact</h2>
 <form method="post">
 	<div class="row">
@@ -24,7 +25,7 @@
 	<div class="row">
 		<div class="col-sm-4">Message</div>
 		<div class="col-sm-10">
-		<input type="text" name="m" class="form-control"/></div>
+		<textarea type="text" name="m" class="form-control"></textarea></div>
 	</div>
 	
 	<div class="row" style="margin-top:10px">
@@ -32,19 +33,7 @@
 		<div class="col-sm-10">
 		<input type="submit" value="Send Query" name="save" class="btn btn-success"/>
 		
-		</div>
-	</div>
 		
-		<!-- </div> -->
-		<div class="col-md-15">
-		<h2>Contact us</h2>
-		
-		Name: Sanjeev Kumar<br/>
-		Mobile: 9015501897<br/>
-		Email:phptpoint@gmail.com<br/>
-		Website: <a href="http://www.phptpoint.com/">Phptpoint</a>
-		</div>
-	<!-- </div> -->
 	
 	
 </form>	
